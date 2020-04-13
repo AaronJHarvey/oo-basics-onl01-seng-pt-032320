ba8d4c4ee2e9a0ac797a7d7732a9dc4f6c3262f1
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-13 22:23:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,21 +1,3 @@
-class Book
-<<<<<<< HEAD
-  attr_accessor :author, :page_count, :genre, :title
-  
- def initialize(title)
- @title = title 
- end 
- 
-def turn_page
-  puts "Flipping the page...wow, you read fast!"
-end
-=======
- def initialize(title)
- end 
- 
- def title 
-   @title = title 
-  end
->>>>>>> 470e8e1073f2f8852d80ddee02e8fbc616387d0e
-end 
+class Book 
 
+end
